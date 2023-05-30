09c4294abacf8a679393486fa9bb6c66b215a85a
Patryk Marczak <m.patryk@outlook.com>
2023-05-30 13:27:47 +0200

    Auth form

--- lib/widgets/authenticate_form.dart
+++ lib/widgets/authenticate_form.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 
 class AuthenticateForm extends StatelessWidget {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final TextEditingController emailController;
   final TextEditingController passwordController;
   final bool isLoading;
   final VoidCallback? handleFormSubmit;
   final String buttonLabel;
-  const AuthenticateForm(
+  AuthenticateForm(
       {super.key,
       required this.emailController,
       required this.passwordController,
@@ -16,40 +17,49 @@ class AuthenticateForm extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SizedBox(
-          width: 300,
-          child: TextField(
-            controller: emailController,
-            keyboardType: TextInputType.emailAddress,
-            decoration: const InputDecoration(
-                border: OutlineInputBorder(), labelText: 'Email'),
-          ),
-        ),
-        SizedBox(
-          width: 300,
-          child: Container(
-            margin: const EdgeInsets.symmetric(vertical: 15),
-            child: TextField(
-              controller: passwordController,
-              obscureText: true,
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          SizedBox(
+            width: 300,
+            child: TextFormField(
+              controller: emailController,
+              keyboardType: TextInputType.emailAddress,
               decoration: const InputDecoration(
-                  border: OutlineInputBorder(), labelText: 'Password'),
+                  border: OutlineInputBorder(), labelText: 'Email'),
             ),
           ),
-        ),
-        SizedBox(
-          width: 300,
-          height: 50,
-          child: ElevatedButton(
-              onPressed: handleFormSubmit,
-              style: const ButtonStyle(),
-              child: isLoading
-                  ? const CircularProgressIndicator()
-                  : Text(buttonLabel)),
-        ),
-      ],
+          SizedBox(
+            width: 300,
+            child: Container(
+              margin: const EdgeInsets.symmetric(vertical: 15),
+              child: TextFormField(
+                controller: passwordController,
+                obscureText: true,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(), labelText: 'Password'),
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 300,
+            height: 50,
+            child: ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    handleFormSubmit!();
+                  }
+                },
+                style: const ButtonStyle(),
+                child: isLoading
+                    ? const CircularProgressIndicator(
+                        color: Colors.white,
+                      )
+                    : Text(buttonLabel)),
+          ),
+        ],
+      ),
     );
   }
 }
